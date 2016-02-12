f761b4d423e1f8464d47ccf7bb471af47427282d
平内真一 <hirauchi.shinichi@classmethod.jp>
2016-02-12 12:27:24 +0900

    すワイプによる翻訳の実装を完了

--- TranslationTwitterSample/ViewController.m
+++ TranslationTwitterSample/ViewController.m
@@ -12,8 +12,6 @@
 #import "Twitter.h"
 #import "Translator.h"
 
-
-
 @interface ViewController()<UITableViewDataSource,UITableViewDelegate,TwitterDelegate,TranslatorDelegate>
 
 
@@ -29,6 +27,8 @@
 @implementation ViewController
 
 
+// 翻訳作業中のtweetのインデックス
+NSIndexPath *convert_in_index = nil;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -43,8 +43,6 @@
 
     // 検索
     [_twitter timeline];
-
-
 }
 
 
@@ -55,18 +53,24 @@
 
 // 翻訳完了
 - (void)didFinishConversion:(NSString *)message {
-    NSLog(message);
+    if (convert_in_index != nil){
+        Tweet *tweet = [_twitter.tweets objectAtIndex: convert_in_index.row];
+        tweet.text = message;
+        [self.tableView reloadRowsAtIndexPaths:[NSArray arrayWithObject:convert_in_index] withRowAnimation:YES];
+        convert_in_index = nil;
+    }
 }
 
-
+// 更新ボタン
 - (IBAction)tapRefreshButton:(id)sender {
     [_twitter timeline];
 }
 
+// 検索ボタン
 - (IBAction)tapSearchButton:(id)sender {
-    NSString *message = @"A swift-flying insectivorous bird with long, slender wings and a superficial resemblance to a swallow, spending most of its life on the wing.Family Apodidae: several genera and numerous species, in particular the common Eurasian swift (Apus apus)";
-
-    [_translator conversion:message];
+//    NSString *message = @"A swift-flying insectivorous bird with long, slender wings and a superficial resemblance to a swallow, spending most of its life on the wing.Family Apodidae: several genera and numerous species, in particular the common Eurasian swift (Apus apus)";
+//
+//    [_translator conversion:message];
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
@@ -88,9 +92,43 @@
     return height * 12 + 50;
 }
 
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+}
+
+
+- (NSArray *)tableView:(UITableView *)tableView editActionsForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+
+    //UITableViewRowAction の backgroundColor プロパティでカスタマイズできる。
+
+    UITableViewRowAction *action = [UITableViewRowAction rowActionWithStyle:UITableViewRowActionStyleDestructive
+                                       title:@"翻訳"
+                                     handler:^(UITableViewRowAction *action, NSIndexPath *indexPath) {
+                                         Tweet *tweet = [_twitter.tweets objectAtIndex:indexPath.row];
+                                         [_translator conversion:tweet.text];
+                                         convert_in_index = indexPath;
+                                     }];
+    action.backgroundColor = [UIColor colorWithRed:0 green:0.2 blue:0 alpha:1];
+    return @[action];
+
+//    return @[
+//             [UITableViewRowAction rowActionWithStyle:UITableViewRowActionStyleDestructive
+//                                                title:@"翻訳"
+//                                              handler:^(UITableViewRowAction *action, NSIndexPath *indexPath) {
+//                                                  Tweet *tweet = [_twitter.tweets objectAtIndex:indexPath.row];
+//                                                  [_translator conversion:tweet.text];
+//                                                  convert_in_index = indexPath;
+//                                              }],
+//             ];
+}
+
 
 @end
